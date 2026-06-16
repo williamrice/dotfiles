90e743a121cf58321cb3621c2f2fcf66eb293729
William Rice <william@williamarice.com>
2026-06-16 12:13:24 -0400

    add sunshine on startup

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -1,15 +1,16 @@
 local M = {}
 
 function M.setup(apps)
-  hl.on("hyprland.start", function()
-    hl.exec_cmd(apps.terminal)
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("mako")
-    hl.exec_cmd("copyq")
-    hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  end)
+	hl.on("hyprland.start", function()
+		hl.exec_cmd(apps.terminal)
+		hl.exec_cmd("waybar")
+		hl.exec_cmd("sunshine")
+		hl.exec_cmd("mako")
+		hl.exec_cmd("copyq")
+		hl.exec_cmd("hyprpaper")
+		hl.exec_cmd("hypridle")
+		hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	end)
 end
 
 return M
