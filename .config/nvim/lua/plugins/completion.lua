7e5b9b8128d9c0dbfe72505257a192b522bec324
Billy Rice <me@billyrice.com>
2026-09-06 13:01:56 -0400

    fix(nvim): configure Lua globals and LazyDev completion

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -13,7 +13,16 @@ cmp.setup({
 		["<C-k>"] = { "scroll_documentation_up", "fallback" },
 	},
 	snippets = { preset = "luasnip" },
-	sources = { default = { "lsp", "path", "snippets", "buffer", "lazydev" } },
+	sources = {
+		default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+		providers = {
+			lazydev = {
+				name = "LazyDev",
+				module = "lazydev.integrations.blink",
+				score_offset = 100,
+			},
+		},
+	},
 	completion = {
 		documentation = { auto_show = true, auto_show_delay_ms = 300 },
 		list = { selection = { preselect = false, auto_insert = false } },
