cceed3477c42954a4772a5d82ca57dda06104b7e
Billy Rice <me@billyrice.com>
2026-09-06 18:12:19 -0400

    fix(nvim): escape nvim-tree filter patterns

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,7 @@ require("nvim-tree").setup({
 	update_focused_file = { enable = true, update_root = false },
 	view = { width = 34, preserve_window_proportions = true },
 	renderer = { group_empty = true, highlight_git = "name", indent_markers = { enable = true } },
-	filters = { dotfiles = false, git_ignored = false, custom = { "^.git$", "^node_modules$", "^.cache$" } },
+	filters = { dotfiles = false, git_ignored = false, custom = { "^\\.git$", "^node_modules$", "^\\.cache$" } },
 	git = { enable = true, ignore = false },
 	diagnostics = { enable = true, show_on_dirs = true },
 })
